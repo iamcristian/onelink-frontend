6709da44ee6577ba6e63d1598f24bea626e5c674
cristian arando <crisarandosyse@gmail.com>
2024-12-31 10:54:59 -0400

    initialize project with React, TypeScript, and Vite setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>hello</div>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
